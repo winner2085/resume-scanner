77862a094074d799728647077b07dd969870d6eb
Erica <ericac2700@tuckahoeschools.org>
2025-03-19 12:27:58 +0000

    victoria

--- scanner.py
+++ scanner.py
@@ -1,13 +1,18 @@
 import pypdf
 from pypdf import PdfReader
+reader = PdfReader("dummy.pdf")
+page = reader.pages[0]
+print(page.extract_text(0))
 
-reader = pypdf.PdfReader('Victoria Gjelaj - First Crusade Questions.pdf')
+pdf_document = "dummy.pdf"
+file_name = pdf_document.split('.')
 
-print(len(reader.pages))
-
-print(reader.pages[0].extract_text())
-
-text = ""
-for page in reader.pages:
-    text += page.extract_text() + "\n"
-print(text)
+with open(pdf_document, "rb") as filehandle, open(pdf_document, mode='w', encoding='UTF-8') as output:
+    pdf = PdfReader(filehandle)
+    num_of_pages = len(pdf.pages)
+    for page_number in range(num_of_pages):
+        page = pdf.pages[page_number]
+        print(f"Page: {page_number+1}", file=output)
+        print('', file=output)
+        print(page.extract_text(), file=output)
+        print('', file=output)
