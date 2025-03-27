f418281a2977071bbbaca408a446f1a92bdac222
Erica <ericac2700@tuckahoeschools.org>
2025-03-27 12:28:01 +0000

    git add .

--- scanner.py
+++ scanner.py
@@ -1,20 +1,23 @@
 import pypdf
 import re
 import warnings
-reader = pypdf.PdfReader("sample-local-pdf.pdf")
-
-# Filter out all warnings, including those from pypdf
-warnings.filterwarnings("ignore")
-
-# To specifically ignore UserWarnings which often include the "wrong pointing object" messages
-warnings.filterwarnings("ignore", category=UserWarning)
+reader = pypdf.PdfReader("resume1.pdf")
 
 num_of_pages = len(reader.pages)
 
 #define key terms
-string1 = "three"
+string1 = "experience"
+string2 = "graduate"
+string3 = "Computer Science"
+string4 = "Engineering"
 
 for page in reader.pages:
     text = page.extract_text()
     res_search = re.search(string1, text)
-    print(res_search)
+    print(text)
+    res_search = re.search(string2, text)
+    print(text)
+    res_search = re.search(string3, text)
+    print(text)
+    res_search = re.search(string4, text)
+    print(text)
